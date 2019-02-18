c2dffc98942e6c12b7570c606df8864611632c61
maipc <a103291@yahoo.com.tw>
2019-02-18 22:12:00 +0800

    add the picture parsed from wiki and animation

--- main.js
+++ main.js
@@ -14,31 +14,39 @@ function getData() {
     .then(response => response.json())
     .then(data => {
         if(data[1]) {
-            console.log(data);
             output.innerHTML='';
             suggest.innerHTML='';
             var item = null;
-            for(var i=0; i<6; i++) {
+            for(var i=0; i<5; i++) {
                 item = document.createElement('li');
-                if(i!==0) {
-                    item.id = 'list';
-                    item.innerHTML = data[1][i-1];
-                }
-                if(i===0) item.innerHTML = '----------您要搜尋的是----------';
+                item.id = 'list';
+                item.innerHTML += data[1][i];
                 suggest.appendChild(item);
             }
             const select = suggest.querySelectorAll('#list');
             select.forEach(function(n, index){
                 n.addEventListener('click', function(e) {
-                    const endpoint = encodeURI(`https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&origin=*&titles=${data[1][index]}`);
+                    const endpoint = encodeURI(`https://en.wikipedia.org/w/api.php?action=query&prop=extracts|pageimages&piprop=original&format=json&exintro=&origin=*&titles=${data[1][index]}`);
                     fetch(`${endpoint}`)
                     .then(res => res.json())
                     .then(context => {
                         var item_id = Object.keys(context.query.pages)[0];
-                        if(context.query.pages[item_id].extract !== "")
-                            output.innerHTML=context.query.pages[item_id].extract;
-                        else
-                        output.innerHTML="<p>There's no context !</p>";
+                        var content = context.query.pages[item_id].extract;
+                        if(content === "") {
+                            output.innerHTML = "<p>undefined</p>";
+                        } else {
+                            if(Object.keys(context.query.pages[item_id]).length > 4) {
+                                var src = context.query.pages[item_id].original.source;
+                                var width = context.query.pages[item_id].original.width;
+                                if(width > 200) width /=10;
+                                var height = context.query.pages[item_id].original.height;
+                                if(height > 200) height /= 10;
+                                output.innerHTML = "<img src=\"" + src + "\" width=\"" + width + "\" height=\"" + height + "\">";
+                                output.innerHTML += content;
+                            } else {
+                                output.innerHTML = content;
+                            }
+                        }
                     })
                 })
             })
